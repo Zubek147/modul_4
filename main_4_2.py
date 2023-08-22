fa7bc4da22e5d1ea181936fd730df499d40398e5
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-08-22 18:37:28 +0200

    Innit project

--- main_4_2.py
+++ main_4_2.py
@@ -1,1 +1,6 @@
-
+#stworzę funkcję określającą czy dane słowo to palindrom
+#jeśli testowane słowo będzie palindromem funkcja zwróci wartość TRUE
+#jeśli testowane słowo nie będzie palindromem funkcja zwróci wartośc FALSE
+#przetestuję słowo, które jest palindromem, żeby sprawdzić czy funkcja działa poprawnie
+#dla pewności sprawdzę kolejne słowo, które jest palindromem
+#przetestuję słowo, które nie jest palindromem, żeby sprawdzić drugi warunek funkcji i poprawność działania
